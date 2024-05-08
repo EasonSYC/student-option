271e9a6d90030da55cd7eaaf1fa68e867efca3ae
Eason Shao <eason.syc@icloud.com>
2024-05-08 15:40:12 +0100

    testing codes. note that teachers & students are generated by gpt.

---
+++ SQL Scripts/Populate Data 1.sql
@@ -0,0 +1,35 @@
+INSERT INTO ExamBoards VALUES ('OCR')
+INSERT INTO ExamBoards VALUES ('AQA')
+INSERT INTO ExamBoards VALUES ('Edexcel')
+INSERT INTO ExamBoards VALUES ('WJEC')
+INSERT INTO ExamBoards VALUES ('CIE')
+GO
+
+INSERT INTO Categories VALUES ('STEM')
+INSERT INTO Categories VALUES ('Arts and Design')
+INSERT INTO Categories VALUES ('Business and Economics')
+INSERT INTO Categories VALUES ('Social Sciences')
+INSERT INTO Categories VALUES ('Languages')
+GO
+
+INSERT INTO Courses VALUES ('Maths', 5, 1)
+INSERT INTO Courses VALUES ('Sciences', 3, 1)
+INSERT INTO Courses VALUES ('Computing', 4, 1)
+GO
+
+INSERT INTO Courses VALUES ('Art and Design', 2, 2)
+INSERT INTO Courses VALUES ('Drama', 2, 2)
+GO
+
+INSERT INTO Courses VALUES ('Economics', 3, 3)
+INSERT INTO Courses VALUES ('Business Studies', 3, 3)
+GO
+
+INSERT INTO Courses VALUES ('Ancient History', 5, 4)
+INSERT INTO Courses VALUES ('Geography', 1, 4)
+GO
+
+INSERT INTO Courses VALUES ('English', 5, 5)
+INSERT INTO Courses VALUES ('German', 3, 5)
+INSERT INTO Courses VALUES ('Greek', 2, 5)
+GO
