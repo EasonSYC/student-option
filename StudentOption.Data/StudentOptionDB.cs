841da86c7a945d2def0690f8d0af61ab7da15feb
Eason Shao <eason.syc@icloud.com>
2024-05-15 10:59:43 +0100

    delete test method.

--- StudentOption.Data/StudentOptionDB.cs
+++ StudentOption.Data/StudentOptionDB.cs
@@ -62,22 +62,5 @@ public class StudentOptionDB(string connectionString) // To-do: OOP
         return classSets;
     }
 
-    public List<string> GetStudentNames()
-    {
-        List<string> names = [];
-        using (SqlConnection connection = new())
-        {
-            connection.ConnectionString = _connectionString;
-            connection.Open();
-            SqlCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT * FROM dbo.Students";
-            var dataReader = command.ExecuteReader();
-            while (dataReader.Read())
-            {
-                string name = $"{dataReader.GetString(1)} {dataReader.GetString(2)}";
-                names.Add(name);
-            }
-        }
-        return names;
-    }
+    
 }
