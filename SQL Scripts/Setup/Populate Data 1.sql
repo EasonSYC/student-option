06933727debdf3dfca262bb221f7de0f0aaa8339
Eason Shao <eason.syc@icloud.com>
2024-05-15 10:43:22 +0100

    fix problem.

--- SQL Scripts/Setup/Populate Data 1.sql
+++ SQL Scripts/Setup/Populate Data 1.sql
@@ -12,24 +12,24 @@ INSERT INTO Categories (CategoryName) VALUES ('Social Sciences')
 INSERT INTO Categories (CategoryName) VALUES ('Languages')
 GO
 
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Maths', 5, 1)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Sciences', 3, 1)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Computing', 4, 1)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Maths', 5, 1)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Sciences', 3, 1)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Computing', 4, 1)
 GO
 
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Art and Design', 2, 2)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Drama', 2, 2)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Art and Design', 2, 2)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Drama', 2, 2)
 GO
 
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Economics', 3, 3)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Business Studies', 3, 3)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Economics', 3, 3)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Business Studies', 3, 3)
 GO
 
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Ancient History', 5, 4)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Geography', 1, 4)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Ancient History', 5, 4)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Geography', 1, 4)
 GO
 
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('English', 5, 5)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('German', 3, 5)
-INSERT INTO Courses (Title, CategoryID, ExamBoardID) VALUES ('Greek', 2, 5)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('English', 5, 5)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('German', 3, 5)
+INSERT INTO Courses (Title, ExamBoardID, CategoryID) VALUES ('Greek', 2, 5)
 GO
