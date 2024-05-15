a1d455cfb53d628676895e25a586dcf119e272d2
Eason Shao <eason.syc@icloud.com>
2024-05-15 15:18:17 +0100

    remove useless usings.

--- StudentOption.Data/StudentOptionDB.cs
+++ StudentOption.Data/StudentOptionDB.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-using System.Globalization;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using StudentOption.Classes;
 
 namespace StudentOption.Data;
