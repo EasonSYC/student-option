b763abb6ef476ce5f65ad7e610b4e26ecc84ddfe
Eason Shao <eason.syc@icloud.com>
2024-06-25 10:43:29 +0100

    no top level statement.

--- StudentOption/Program.cs
+++ StudentOption/Program.cs
@@ -3,6 +3,14 @@
 using StudentOption.Application;
 using Microsoft.Extensions.Configuration;
 
-var config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
-string connectionString = config["ConnectionStrings:studentDb"] ?? string.Empty;
-new TerminalApplication(connectionString).MainInterface();
+namespace StudentOption;
+
+internal class Program
+{
+    static void Main()
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+        string connectionString = config["ConnectionStrings:studentDb"] ?? string.Empty;
+        new TerminalApplication(connectionString).MainInterface();
+    }
+}
